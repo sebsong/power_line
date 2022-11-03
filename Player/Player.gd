5e50a3c0750a105d882c1e02e53b20cfbf4818a9
Sebastian Song <sebsong@berkeley.edu>
2022-11-03 11:46:48 -0700

    power node placment

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const POWER_NODE_SCENE : PackedScene = preload("res://Scenes/PowerNode.tscn")
+
 var speed = 500
 
 # Called when the node enters the scene tree for the first time.
@@ -9,6 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	move(delta)
+	power_node_placement()
+
+func move(delta):
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
@@ -17,3 +23,9 @@ func _process(delta):
 		position.x -= speed * delta
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta
+	
+func power_node_placement():
+	if Input.is_action_just_pressed("place_power_node"):
+		var power_node : Node = POWER_NODE_SCENE.instance()
+		power_node.position = position
+		get_parent().add_child(power_node)
