35be6f377039a9ac69839a53fc598b328a931f73
Sebastian Song <sebsong@berkeley.edu>
2022-11-03 11:04:33 -0700

    player scene and movement

--- Player.gd
+++ Player.gd
@@ -1,10 +1,6 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +8,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
